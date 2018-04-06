13093133b51016bc0739bfadbb0cd4ed98697537
huongvt789 <huongvt52@wru.vn>
2018-04-06 17:06:58 +0700

    decribe math

---
+++ life.js
@@ -0,0 +1,14 @@
+//map:
+	//size
+		//rows, colums
+	//sells
+//sell
+	//non-life
+		//calculateStateCell
+		//calculateNeghtBorNumber.
+		//calculateNextTime
+	//has-life
+
+//=>Xu ly dong thay doi kich thuoc ten template .html 
+//(cho phep nhap kich thuoc vao ma tran thay doit theo)
+
