0fe4d0ca8c5bbdbe574d39b906ea0a2efdb1683a
huongvt789 <huongvt52@wru.vn>
2018-04-08 15:26:35 +0700

    clcik event creatMap

--- life.js
+++ life.js
@@ -1,6 +1,6 @@
 //map:
 	//size
-		//rows, colums
+		//rows, colums.
 	//sells
 //sell
 	//non-life
@@ -11,4 +11,31 @@
 
 //=>Xu ly dong thay doi kich thuoc ten template .html 
 //(cho phep nhap kich thuoc vao ma tran thay doit theo)
+var createMap = [];
 
+new Vue ({
+	el: '#map',
+	data: {
+		createUniverse: createMap,
+		isCreat: true,
+	},
+
+	methods: {
+		createNewMap: function () {
+			for (var i = 0; i<100; i++){
+				this.createUniverse[i] = [];
+				for (var j = 0; j <100; j++) {
+					this.createUniverse[i][j] = Math.round (Math.random());
+				}
+			}
+			return this.createUniverse;
+		}
+
+//add function...
+	},
+	computed: {
+		createNewUnverse: function () {
+			return this.creatNewMap();
+		},
+	}
+});
