625bd602385923513d74ddcf81e2f5b9867108bb
huongvt789 <huongvt52@wru.vn>
2018-04-10 17:09:37 +0700

    created numbermap

--- life.js
+++ life.js
@@ -11,59 +11,60 @@
 
 //=>Xu ly dong thay doi kich thuoc ten template .html 
 //(cho phep nhap kich thuoc vao ma tran thay doit theo)
-var numberUniverseMap = [];
-	window.onload = function () {
-	var map = new Vue ({
-		el: '#map',
-		data: {
-			createUniverse: numberUniverseMap,
-			isCreatMap: true,
-			isShowMap: false
-		},
 
-		methods: {
-			createNewMap: function () {
-				for (var i = 0; i<100; i++){
-					this.createUniverse[i] = [];
-					for (var j = 0; j <100; j++) {
-						this.createUniverse[i][j] = Math.round (Math.random());
-					}
-				}
-				return this.createUniverse;
-			},
-
-	//add function...
-			getValueOfCell: function (x,y) {
-				if (x < 0 || x >= 100 || y <0 || y >=100) {
-					return 0;
-				}
-				else {
-					return this.createUniverse[x][y];
-				}
-			},
-			countNeighPlanet: function (i, j) {
-	            return parseInt(this.getValueOfCell((i - 1), (j - 1))) + parseInt(this.getValueOfCell((i - 1), j)) + parseInt(this.getValueOfCell((i - 1), (j + 1))) + parseInt(this.getValueOfCell(i, (j - 1))) + parseInt(this.getValueOfCell(i, (j + 1))) + parseInt(this.getValueOfCell((i + 1), (j - 1))) + parseInt(this.getValueOfCell((i + 1), j)) + parseInt(this.getValueOfCell((i + 1), (j + 1))) ;
-			},
-			calculateStateCell: function (i, j) {
-				this.isShowMap = false;
-				if (this.countNeighPlanet (i, j) <2 || this.countNeighPlanet (i, j) >3) {
-					this.createUniverse[i][j];
-				}
-			}
-		},
-		computed: {
-			createNewUnverse: function () {
-				return this.creatNewMap();
-			},
-			universeNumberMap: function () {
-				this.isShowMap =false;
-				for ( var i = 0; i < 100; i++) {
-					for ( var j = 0; j <100; j++) {
-						this.calculateStateCell (i, j);
-					}
-				}
-				return this.createUniverse;
-			}
-		}
-	});
+var createMap = [];
+window.onload = function () {
+var map = new Vue ({
+        el: '#map',
+        data: {
+            newUniverse: createMap,
+            isCreate: true,
+            isShowingMap: false,
+            isShowingNewMap: false,
+        },
+        methods: {
+            addNewMap: function() {
+                for (var i = 0 ; i < 70; i++) {
+                    this.newUniverse[i] = [];
+                    for (var j = 0; j < 70; j++) {
+                        this.newUniverse[i][j] = Math.round(Math.random());
+                    }
+                }
+                return this.newUniverse;
+            },
+            getValueOfCell: function (x, y) {
+                if (x < 0 || x >= 70 || y < 0 || y >= 70) {
+                    return 0;
+                }
+                else {
+                    return this.newUniverse[x][y];
+                }
+            },
+            countNeighborPlanet: function (i, j) {
+                return parseInt(this.getValueOfCell((i - 1), (j - 1))) + parseInt(this.getValueOfCell((i - 1), j)) + parseInt(this.getValueOfCell((i - 1), (j + 1))) + parseInt(this.getValueOfCell(i, (j - 1))) + parseInt(this.getValueOfCell(i, (j + 1))) + parseInt(this.getValueOfCell((i + 1), (j - 1))) + parseInt(this.getValueOfCell((i + 1), j)) + parseInt(this.getValueOfCell((i + 1), (j + 1))) ;
+            },
+            calculateCellState: function (i, j) {
+                this.isShowingMap = false;
+                if (this.countNeighborPlanet(i, j) < 2 || this.countNeighborPlanet(i, j) > 3) {
+                    this.newUniverse[i][j] = 0;
+                } else if (this.countNeighborPlanet(i, j) == 3) {
+                    this.newUniverse[i][j] = 1;
+                }
+            },
+        },
+        computed: {
+            newMapUniverse: function () {
+                return this.addNewMap();
+            },
+            universeMapNumber: function () {
+                this.isShowingMap = false;
+                for (var i = 0 ; i < 70; i++) {
+                    for (var j = 0; j < 70; j++) {
+                        this.calculateCellState(i, j);
+                    }
+                }
+                return this.newUniverse;
+            },
+        }
+    });
 }
