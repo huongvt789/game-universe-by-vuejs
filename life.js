af360c5ca5b575144b69c9af7cee98b33d81401e
huongvt789 <huongvt52@wru.vn>
2018-04-13 11:07:31 +0700

    create map2

--- life.js
+++ life.js
@@ -1,13 +1,13 @@
 //map:
-	//size
-		//rows, colums.
-	//sells
+//size
+//rows, colums.
+//sells
 //sell
-	//non-life
-		//calculateCellState
-		//calculateNeghtBorNumber.
-		//calculateNextTime
-	//has-life
+//non-life
+//calculateCellState
+//calculateNeghtBorNumber.
+//calculateNextTime
+//has-life
 
 //=>Xu ly dong thay doi kich thuoc ten template .html 
 //(cho phep nhap kich thuoc vao ma tran thay doit theo)
@@ -16,20 +16,19 @@
 // Start new Vue: Khai bao cac method: thuc hien cong viec gi (thogn)
 var createMap = [];
 window.onload = function () {
-var map = new Vue ({
+    var map = new Vue({
         el: '#map-universe',
         data: {
             newUniverse: createMap,
             isCreate: true,
-            isShowMap: false,
+            isShowMap: true,
             //next.
             isCreateNextMap: true,
-            isShowNextMap1: false,
-            isShowNextMap2: false
+            isShowNextMap1: false
         },
         methods: {
-            addNewMap: function() {
-                for (var i = 0 ; i < 70; i++) {
+            addNewMap: function () {
+                for (var i = 0; i < 70; i++) {
                     this.newUniverse[i] = [];
                     for (var j = 0; j < 70; j++) {
                         this.newUniverse[i][j] = Math.round(Math.random());
@@ -48,7 +47,7 @@ var map = new Vue ({
             },
 
             countNeighborPlanet: function (i, j) {
-                return parseInt(this.getValueOfCell((i - 1), (j - 1))) + parseInt(this.getValueOfCell((i - 1), j)) + parseInt(this.getValueOfCell((i - 1), (j + 1))) + parseInt(this.getValueOfCell(i, (j - 1))) + parseInt(this.getValueOfCell(i, (j + 1))) + parseInt(this.getValueOfCell((i + 1), (j - 1))) + parseInt(this.getValueOfCell((i + 1), j)) + parseInt(this.getValueOfCell((i + 1), (j + 1))) ;
+                return parseInt(this.getValueOfCell((i - 1), (j - 1))) + parseInt(this.getValueOfCell((i - 1), j)) + parseInt(this.getValueOfCell((i - 1), (j + 1))) + parseInt(this.getValueOfCell(i, (j - 1))) + parseInt(this.getValueOfCell(i, (j + 1))) + parseInt(this.getValueOfCell((i + 1), (j - 1))) + parseInt(this.getValueOfCell((i + 1), j)) + parseInt(this.getValueOfCell((i + 1), (j + 1)));
             },
 
             calculateCellState: function (i, j) {
@@ -60,45 +59,22 @@ var map = new Vue ({
             },
 
             changeUniverseMapNumber1: function () {
-            	this.isShowNextMap1 = true;
-            	this.isShowNextMap2 = true;
-            	for (var i = 0; i < 70; i++ ){
-            		for (var j = 0; j < 70; j++){
-            			this.calculateCellState (i, j);
-            		}
-            	}
-            	return this.newUniverse;
-            },
-
-            changeUniverseMapNumber2: function () {
-            	this.isShowNextMap1 = false;
-            	this.isShowNextMap2 = true;
-            	for (var i = 0; i < 70; i++){
-            		for (var j = 0; j < 70; j++){
-            			this.calculateCellState (i, j);
-            		}
-            	}
-            	return this.newUniverse;
-            }
-
-        },
-        computed: {
-            newMapUniverse: function () {
-                return this.addNewMap();
-            },
-
-            universeMapNumber: function () {
-                for (var i = 0 ; i < 70; i++) {
+                this.isShowNextMap1 = true;
+                for (var i = 0; i < 70; i++) {
                     for (var j = 0; j < 70; j++) {
                         this.calculateCellState(i, j);
                     }
                 }
                 return this.newUniverse;
             },
-
+        },
+        computed: {
+            newMapUniverse: function () {
+                return this.addNewMap();
+            },
             changeNewMapNumber: function () {
-            	return this.changeUniverseMapNumber1 ();
+                return this.changeUniverseMapNumber1();
             }
-        } 
+        }
     });
 }
